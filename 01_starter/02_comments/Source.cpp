54c64834cfc9c487a99a17097ee96137b9df93fb
Michał Maj <icecheater@gmail.com>
2022-08-29 19:44:18 +0200

    comments

---
+++ 01_starter/02_comments/Source.cpp
@@ -0,0 +1,26 @@
+/*
+ * Comments let's us to put some messages to other developers
+ * Comments are human readable documentation code
+ */
+
+
+// Standard library header
+// This header is part of the Input / output library.
+#include <iostream>
+
+
+int main()
+{
+	// one line comment
+
+	/*
+	 * Multi-line comment block
+	 * We can write as many line
+	 * of comments as we want!
+	 */
+
+	// print some values
+	std::cout << "Hello C++20!" << std::endl;
+
+	return 0;
+}
