4e5f5af5a22458403d3deb3c68182ed4be34897e
Michał Maj <michalpmaj@gmail.com>
2022-09-05 19:39:05 +0200

    implicit data converion

--- 05_overflow_and_underflow/01_implicit_data_conversion/Source.cpp
+++ 05_overflow_and_underflow/01_implicit_data_conversion/Source.cpp
@@ -6,6 +6,9 @@
  * all int's will be converted to doubles.
  * Second rule "Smaller types will be converted to bigger type in memory sizes".
  * Implicit conversions are done by the compiler without our involvement.
+ *
+ * There is an exception to this rule. When we do an assignment operation it is
+ * possible to cause implicit narrowing conversion.
  */
 
 #include <iostream>
@@ -13,6 +16,23 @@
 
 int main()
 {
+	double price{ 45.6 };
+	int units{ 10 };
+
+	auto total_price = price * units;
+
+	std::cout << "Total price: " << total_price << std::endl;
+	// It will be double - 8 bytes
+	std::cout << "sizeof total_price: " << sizeof(total_price)<< std::endl;
+
+	int x;
+	double y{ 45.44 };
+	// This operation (assignment) will cause an implicit narrowing conversion.
+	// y will be converted to int before assignment.
+	x = y;
+	std::cout << "The value of x: " << x << std::endl;
+	// It will be int - 4 bytes
+	std::cout << "sizeof x: " << sizeof(x) << std::endl;
 
 	return 0;
 }
