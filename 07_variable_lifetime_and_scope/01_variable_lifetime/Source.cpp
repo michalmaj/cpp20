81c0cd7692691855c4f865f3b05af36599bd78dd
Michał Maj <michalpmaj@gmail.com>
2022-09-09 19:13:55 +0200

    variable filetime

---
+++ 07_variable_lifetime_and_scope/01_variable_lifetime/Source.cpp
@@ -0,0 +1,26 @@
+/*
+ * Storage duration:
+ *	- local
+ *	- static
+ *	- dynamic
+ */
+
+#include <iostream>
+
+int static_var2{ 80 }; // static duration - dies when program ends
+
+void some_function()
+{
+	int local_var3{ 30 }; // local duration - dies at end of block
+	static int static_var1{ 40 }; // static duration - dies when program ends
+}
+
+int main()
+{
+	int local_var1{ 60 }; // local duration - dies at end of block
+	{
+		int local_var3{ 20 }; // local duration - dies at end of block
+	}
+
+	return 0;
+}
