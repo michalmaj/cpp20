a808f05c666c4152c4d79832c6abfe96a93c3c61
Michał Maj <michalpmaj@gmail.com>
2022-09-10 19:50:59 +0200

    int as logic condition

---
+++ 08_control_flow/05_integral_logic_conditions/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	int condition{ -3 };
+
+	// If 0 then false otherwise true
+	if (condition)
+		std::cout << "We have a " << condition << " in our variable\n";
+	else
+		std::cout << "We have a " << condition << " in our variable\n";
+
+	return 0;
+}
