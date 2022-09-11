dc56ce2f02e0276277f1d516ddfb8b0d1b7642b7
Michał Maj <michalpmaj@gmail.com>
2022-09-11 20:12:08 +0200

    loop with multiple declaration

---
+++ 09_loops/02_for_loop_multiple_declarations/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main()
+{
+
+	for(size_t i{0}, x{5}, y{22}; y > 15; ++i, x+=5, y--)
+	{
+		std::cout << "i: " << i << ", x: " << x << ", y: " << y << std::endl;
+	}
+
+	return 0;
+}
