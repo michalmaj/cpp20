2e1ba8acdc1acc385c1f59b2bcb4c86266b8d2a2
Michał Maj <michalpmaj@gmail.com>
2022-09-12 19:23:23 +0200

    infinite loops

---
+++ 09_loops/08_infinite_loops/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+
+int main()
+{
+	//// Infinite loop: for loop
+	//// Uncomment if you want to see output
+	//for(size_t i{}; ;++i)
+	//{
+	//	std::cout << i << ", I love C++" << std::endl;
+	//}
+
+	//// Infinite loop: while loop
+	//// Uncomment if you want to see output
+	//size_t i{ 0 };
+	//while (true)
+	//{
+	//	std::cout << i << ", I love C++" << std::endl;
+	//	++i; 
+	//}
+
+
+	//// Infinite loop: do while loop
+	//// Uncomment if you want to see output
+	//size_t i{ 0 };
+	//do
+	//{
+	//	std::cout << i << ", I love C++" << std::endl;
+	//	++i;
+	//} while (true);
+
+
+	return 0;
+}
