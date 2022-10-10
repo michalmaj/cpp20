ac447d6be06baeb4503288cdfd5b9eade16aa5dc
Michał Maj <michalpmaj@gmail.com>
2022-10-10 18:20:14 +0200

    pass by pointer to const

--- 14_functions/07_pass_by_pointer_to_const/Source.cpp
+++ 14_functions/07_pass_by_pointer_to_const/Source.cpp
@@ -7,7 +7,7 @@ int main() {
     int age{ 23 }; // Local
     std::cout << "age (before call) : " << age << "&age : " << &age << std::endl; // 23
     say_age(&age); // During passing by pointer we have to pass an address (&) 
-    std::cout << "age (after call) : " << age << "&age : " << &age << std::endl; // 24
+    std::cout << "age (after call) : " << age << "&age : " << &age << std::endl; // 23
 
     return 0;
 }
@@ -15,5 +15,5 @@ int main() {
 
 void say_age(const int* age) { // Parameter
     //++(*age); // We can't modify the value of pointer to const
-    std::cout << "Hello , you are " << *age << " years old! &age : " << age << std::endl; // 24
+    std::cout << "Hello , you are " << *age << " years old! &age : " << age << std::endl; // 23
 }
