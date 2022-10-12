23b75062c4de7d3ae883dca8a02c0bf3557d5cd7
Michał Maj <michalpmaj@gmail.com>
2022-10-12 17:47:44 +0200

    passing sized arrays by reference

---
+++ 14_functions/13_passing_sized_arrays_by_reference/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+//Declaration
+// Force compiler to check if give array has exactly 10 elements
+double sum(const double(&scores)[10]);
+
+
+int main() {
+
+    double student_scores[]{ 10.0,20.0,30.0,40.0,50.0,60.0,70.0,80.0,90.0,1 };
+
+    double sum_result = sum(student_scores);
+
+    std::cout << "result is : " << sum_result << std::endl;
+
+    return 0;
+}
+
+//Definition
+double sum(const double(&scores)[10]) {
+    double sum{};
+    for (size_t i{}; i < std::size(scores); ++i) {
+        sum += scores[i];
+    }
+    return sum;
+}
