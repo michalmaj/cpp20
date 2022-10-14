1fdfd29ed5bdc6c06883d14b840dc8fa08f751a8
Michał Maj <michalpmaj@gmail.com>
2022-10-14 19:55:26 +0200

    enumerated types

--- 15_enums_and_type_aliases/01_enumerated_types/Source.cpp
+++ 15_enums_and_type_aliases/01_enumerated_types/Source.cpp
@@ -8,7 +8,7 @@ enum class Month : unsigned char // change type to unsigned char (can represent 
 {
 	Jan=1, January=1, Feb, Mar, // In this case January is the same as Jan
 	Apr, May, Jun,
-	Jul, Aug, Sep, September=Sep, // Better version for making two names be the same
+	Jul, Aug, Sep, September=Sep, // Better version for making two names to be the same
 	Oct=20, Nov, Dec // Because Oct is 20, Dec will be 22
 };
 
