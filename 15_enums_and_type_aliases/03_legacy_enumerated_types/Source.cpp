3d97262373823df7172d83c95276ea168d5a1ed4
Michał Maj <michalpmaj@gmail.com>
2022-10-14 20:46:25 +0200

    legacy enumerated types

---
+++ 15_enums_and_type_aliases/03_legacy_enumerated_types/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+enum Direction : unsigned char {
+    TopLeft = 64, TopRight, Center, BottomLeft, BottomRight
+};
+
+enum Tool {
+    Pen, Marker, Eraser, Rectangle, Circle, PaintBucket
+};
+
+
+int main() {
+
+    Direction direction{ Direction::TopLeft };
+    Tool tool{ Tool::Pen };
+
+    std::cout << "direction : " << static_cast<unsigned int>(direction) << std::endl;
+    std::cout << "tool : " << tool << std::endl;
+
+    // Using old enum we can compare different enums, which is BAD!
+    // But if we change to enum class we will get a Compiler Error
+    std::cout << "(tool > direction) : " << (tool > direction) << std::endl;
+
+    return 0;
+}
