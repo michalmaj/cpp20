4f7948353c7b9f9aba10a18150b92e0b4419476f
Michał Maj <michalpmaj@gmail.com>
2022-10-21 20:39:14 +0200

    overloading with const parameters by value

---
+++ 18_function_overloading/04_overloading_with_const_parameters_by_value/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+// Those functions are equivalent - so we have a redefinition! We need to comment out one of them
+int max(const int a, const int b)
+{
+    return (a > b)? a : b;
+}
+
+//int max(int a, int b)
+//{
+//    ++a;
+//    return (a > b) ? a : b;
+//}
+
+int main() {
+
+
+    return 0;
+}
+
