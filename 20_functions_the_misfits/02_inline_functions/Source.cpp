ab54b1cc5836868be32e095f2966e3d4b43473cc
Michał Maj <michalpmaj@gmail.com>
2022-10-23 20:38:14 +0200

    inline functions

--- 20_functions_the_misfits/02_inline_functions/Source.cpp
+++ 20_functions_the_misfits/02_inline_functions/Source.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+inline int max(int a, int b)
+{
+	if (a > b)
+		return a;
+	return b;
+}
+
 int main()
 {
+	int a{ 5 };
+	int b{ 8 };
+
+	std::cout << "max: " << max(a, b) << std::endl;
 
 	return 0;
 }
