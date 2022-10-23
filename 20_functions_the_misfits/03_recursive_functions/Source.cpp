d59e08b4d5d8988d6fc6faa392a4da13e2f114e0
Michał Maj <michalpmaj@gmail.com>
2022-10-23 20:44:19 +0200

    recursive functions

---
+++ 20_functions_the_misfits/03_recursive_functions/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+size_t sum_up_to_zero(size_t value)
+{
+	if (value != 0)
+		return value + sum_up_to_zero(value - 1);
+	return 0;
+}
+
+int main()
+{
+	std::cout << "result: " << sum_up_to_zero(5) << std::endl;
+
+	return 0;
+}
