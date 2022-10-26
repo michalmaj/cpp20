92951751c3ef563b6a8391aac5922f73c16758e3
Michał Maj <michalpmaj@gmail.com>
2022-10-26 18:25:12 +0200

    function templates with multiple parameters

--- 21_function_templates/06_function_templates_with_multiple_parameters/Source.cpp
+++ 21_function_templates/06_function_templates_with_multiple_parameters/Source.cpp
@@ -2,13 +2,13 @@
 #include <typeinfo>
 #include <variant>
 
-/*
-//Problematic : return depends on the order of the template arguments : BAD!
-template <typename T , typename P>
-P   maximum( T a, P b){
-    return ((a > b) ? a : b);
-}
-*/
+
+////Problematic : return depends on the order of the template arguments : BAD!
+//template <typename T , typename P>
+//P   maximum( T a, P b){
+//    return ((a > b) ? a : b);
+//}
+
 
 template <typename ReturnType, typename T, typename P>
 //template < typename T,typename ReturnType , typename P> 
