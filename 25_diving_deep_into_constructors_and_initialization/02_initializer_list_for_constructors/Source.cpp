a74ff350c71c996197319b6da84970a2d766edc1
Michał Maj <michalpmaj@gmail.com>
2022-11-05 20:08:30 +0100

    initializer list for constructors

--- 25_diving_deep_into_constructors_and_initialization/02_initializer_list_for_constructors/Source.cpp
+++ 25_diving_deep_into_constructors_and_initialization/02_initializer_list_for_constructors/Source.cpp
@@ -1,3 +1,19 @@
+/*
+ * Member wise copy vs Initializer lists
+ *
+ * Member wise copy:
+ *	- Two steps:
+ *		1. object creation;
+ *		2. member variable assignment.
+ *	- Potential unnecessary copies of data.
+ *	- Order of member variables doesn't matter.
+ *
+ * Initializer list:
+ *	- Initialization happens at real object creation.
+ *	- Unnecessary copies avoided.
+ *	- Order of member variables matters.
+ */
+
 #include <iostream>
 #include "cylinder.h"
 
