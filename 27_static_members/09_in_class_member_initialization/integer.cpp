6eee48da1b23d247259e7f47f7e8f41cebe40931
Michał Maj <michalpmaj@gmail.com>
2022-11-10 21:21:17 +0100

    in class member initialization

---
+++ 27_static_members/09_in_class_member_initialization/integer.cpp
@@ -0,0 +1,10 @@
+#include "integer.h"
+
+Integer::Integer(int value) : inner_int(value)
+{
+	std::cout << "Constructor for Integer " << inner_int << " called" << std::endl;
+}
+
+Integer::~Integer()
+{
+}
---
+++ 27_static_members/09_in_class_member_initialization/integer.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+
+class Integer
+{
+public:
+	explicit Integer(int value);
+	Integer() {
+		std::cout << "Default constructor for Integer called" << std::endl;
+	}
+	~Integer();
+
+	int get_value() const {
+		return inner_int;
+	}
+
+	void set_value(int new_val) {
+		inner_int = new_val;
+	}
+
+private:
+	int inner_int{};
+
+};
+
+
