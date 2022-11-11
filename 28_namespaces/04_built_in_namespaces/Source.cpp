1926578d0c9eb9f0395132a17d5e7da0b3dd603e
Michał Maj <michalpmaj@gmail.com>
2022-11-11 21:16:59 +0100

    built in namespaces

---
+++ 28_namespaces/04_built_in_namespaces/Source.cpp
@@ -0,0 +1,15 @@
+/*
+ * Documentation:
+ * https://en.cppreference.com/w/cpp/header
+ */
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+	std::string message{ "Hello World" };
+	std::cout << message << std::endl;
+
+	return 0;
+}
