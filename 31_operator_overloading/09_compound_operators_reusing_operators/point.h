f583604531fa1813056129c2669206d7205f05a0
Michał Maj <michalpmaj@gmail.com>
2022-11-24 20:03:01 +0100

    compound operators

---
+++ 31_operator_overloading/09_compound_operators_reusing_operators/point.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <iostream>
+
+class Point
+{
+	friend std::ostream& operator<<(std::ostream& out, const Point& p);
+	friend Point operator+=(Point& left, const Point& right);
+	friend Point operator-=(Point& left, const Point& right);
+	friend Point operator+(const Point& left, const Point& right);
+	friend Point operator-(const Point& left, const Point& right);
+
+public:
+	Point();
+	Point(double x, double y);
+	~Point() = default;
+
+	double lenght() const;
+
+private:
+	double m_x;
+	double m_y;
+};
+
+inline std::ostream& operator<<(std::ostream& out, const Point& p)
+{
+	out << "Point [ x : " << p.m_x << ", y : " << p.m_y << "]";
+	return out;
+}
+
+inline Point operator+=(Point& left, const Point& right)
+{
+	return { left.m_x += right.m_x, left.m_y += right.m_y };
+}
+
+inline Point operator-=(Point& left, const Point& right)
+{
+	return { left.m_x -= right.m_x, left.m_y -= right.m_y };
+}
+
+inline Point operator+(const Point& left, const Point& right)
+{
+	//return { left.m_x + right.m_x, left.m_y + right.m_y };
+	Point p{ left };
+	return p += right;
+}
+inline Point operator-(const Point& left, const Point& right)
+{
+	//return { left.m_x - right.m_x, left.m_y - right.m_y };
+	Point p{ left };
+	return p -= right;
+}
