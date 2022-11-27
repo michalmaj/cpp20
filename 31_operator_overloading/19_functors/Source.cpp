2b7deadfb793fb8971e02ebe67fa22f5dd65688b
Michał Maj <michalpmaj@gmail.com>
2022-11-27 20:56:28 +0100

    funktors

---
+++ 31_operator_overloading/19_functors/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <format>
+
+class Print
+{
+public:
+	void operator()(const std::string& name) const
+	{
+		std::cout << "The name is: " << name << std::endl;
+	}
+
+	std::string operator()(const std::string& last_name, const std::string& first_name) const
+	{
+		return std::format("{} {}", last_name, first_name);
+	}
+};
+
+void do_something(const Print& printer)
+{
+	printer("Snow");
+}
+
+int main()
+{
+	Print print;
+
+	print("John");
+	do_something(print);
+	std::cout << print("Snow", "John") << std::endl;
+
+
+	return 0;
+}
