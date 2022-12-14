1e4c6e1fee90de113e160295b78fb985aa27f6e7
Michał Maj <michalpmaj@gmail.com>
2022-12-14 21:10:07 +0100

    final and override are not keywords

---
+++ 34_polymorphism/10_final_and_override_are_not_keywords/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class final
+{
+	
+};
+
+int main()
+{
+	//double final{ 3.4 };
+	//std::cout << "final: " << final << std::endl;
+
+	//std::string override{ "Hello" };
+	//std::cout << "override: " << override << std::endl;
+
+	final f1;
+
+	return 0;
+}
