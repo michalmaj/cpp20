783d09b396cec2a7086334573c8044778b9eb783
Michał Maj <michalpmaj@gmail.com>
2022-12-21 21:36:26 +0100

    handling exceptions at different levels

--- 35_exception_handling/03_handling_exceptions_at_different_levels/Source.cpp
+++ 35_exception_handling/03_handling_exceptions_at_different_levels/Source.cpp
@@ -4,22 +4,40 @@ void f1();
 void f2();
 void f3();
 
+//void exception_thrower()
+//{
+//	std::cout << "starting execution_thrower()" << std::endl;
+//	throw 0; // Execution will halt from here
+//	std::cout << "ending execution_thrower()" << std::endl;
+//}
+
+// Another way is to handle exception directly in exception_thrower
 void exception_thrower()
 {
 	std::cout << "starting execution_thrower()" << std::endl;
-	throw 0; // Execution will halt from here
+	try
+	{
+		throw 0; // Execution will halt from here
+	}catch (int& ex)
+	{
+		std::cout << "Something went wrong" << std::endl;
+	}
 	std::cout << "ending execution_thrower()" << std::endl;
 }
 
 int main()
 {
-	try
-	{
-		f1();
-	}catch(int& ex)
-	{
-		std::cout << "Handling execution in main()" << std::endl;
-	}
+	// for exception handling in functions
+	f1();
+
+	// For exception handling in main
+	//try
+	//{
+	//	f1();
+	//}catch(int& ex)
+	//{
+	//	std::cout << "Handling execution in main()" << std::endl;
+	//}
 
 	std::cout << "END" << std::endl;
 	return 0;
