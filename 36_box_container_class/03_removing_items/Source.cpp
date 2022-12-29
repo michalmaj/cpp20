0acb8e6834419cd60b63b756c04be8986d75bf98
Michał Maj <michalpmaj@gmail.com>
2022-12-29 20:41:38 +0100

    removing items

---
+++ 36_box_container_class/03_removing_items/Source.cpp
@@ -0,0 +1,56 @@
+#include "boxcontainer.h"
+
+int main()
+{
+	BoxContainer box1{ 5 };
+	std::cout << "box1: " << box1 << std::endl;
+
+	box1.add(11);
+	box1.add(12);
+	box1.add(13);
+	std::cout << "box1: " << box1 << std::endl;
+
+	box1.add(14);
+	box1.add(15);
+	std::cout << "box1: " << box1 << std::endl;
+
+	box1.add(16);
+	std::cout << "box1: " << box1 << std::endl;
+
+	for (size_t i{ 0 }; i < 4; ++i)
+		box1.add(i + 17);
+
+	std::cout << "box1: " << box1 << std::endl;
+
+	box1.add(21);
+	std::cout << "box1: " << box1 << std::endl;
+
+	std::cout << "\n======================================================\n\n";
+
+	std::cout << "Removing items:" << std::endl;
+
+	box1.remove_item(15);
+	std::cout << "box1: " << box1 << std::endl;
+
+	box1.remove_item(30);
+	std::cout << "box1: " << box1 << std::endl;
+
+	box1.remove_item(18);
+	std::cout << "box1: " << box1 << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "Removing all instances of an item:" << std::endl;
+
+	box1.add(11);
+	box1.add(33);
+	box1.add(52);
+	box1.add(11);
+
+	std::cout << "box1: " << box1 << std::endl;
+
+	box1.remove_all(11);
+
+	std::cout << "box1: " << box1 << std::endl;
+
+	return 0;
+}
---
+++ 36_box_container_class/03_removing_items/boxcontainer.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "stream_insertable.h"
+
+class BoxContainer : public StreamInsertable
+{
+	using value_type = int;
+
+	static constexpr size_t DEFAULT_CAPACITY{ 30 };
+	static constexpr size_t EXPAND_STEPS{ 5 }; // The box expands the capacity by this amount
+
+public:
+	BoxContainer(size_t capacity = DEFAULT_CAPACITY);
+	BoxContainer(const BoxContainer& source);
+	virtual ~BoxContainer();
+
+	// StreamInsertable Interface
+	virtual void stream_insert(std::ostream& out) const override;
+
+	// Helper getters methods
+	size_t size() const { return m_size; }
+	size_t capacity() const { return m_capacity; }
+
+	// Method to add items to the collection
+	void add(const value_type& item);
+
+	// Remove items
+	bool remove_item(const value_type& item);
+	size_t remove_all(const value_type& item);
+
+private:
+	value_type* m_items;
+	size_t m_capacity;
+	size_t m_size;
+
+	void expand(size_t new_capacity);
+};
+
