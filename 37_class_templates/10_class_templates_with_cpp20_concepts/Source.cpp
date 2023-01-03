8aa229a742b43a0cab183283acf25dc36b638ee2
Michał Maj <michalpmaj@gmail.com>
2023-01-03 21:44:31 +0100

    class templates with C++20 concepts

---
+++ 37_class_templates/10_class_templates_with_cpp20_concepts/Source.cpp
@@ -0,0 +1,46 @@
+#include "boxcontainer.h"
+#include <type_traits>
+
+
+template <typename T>
+requires std::is_arithmetic_v<T>
+class Point
+{
+public:
+	Point() = default;
+	Point(T x, T y) : m_x(x), m_y(y) {}
+
+	friend std::ostream& operator<<(std::ostream& out, const Point<T> operand)
+	{
+		out << "Point [x: " << operand.m_x << ", y: " << operand.m_y << "]";
+		return out;
+	}
+
+private:
+	T m_x;
+	T m_y;
+};
+
+int main()
+{
+	// These two will work perfectly, because int and double are arithmetic
+	Point<int> point_int{ 10, 20 };
+	std::cout << "point_int: " << point_int << std::endl;
+
+	Point<double> point_double{ 11.1, 12.2 };
+	std::cout << "point_int: " << point_double << std::endl;
+
+	//// This piece of code will not work - static_assert
+	//Point<std::string> point_string{ "Hello", "World" };
+	//std::cout << "point_string: " << point_string << std::endl;
+
+	BoxContainer<Point<int>> point_box;
+	point_box.add(point_int);
+	point_box.add(Point<int>{40, 50});
+	std::cout << "point_box: " << point_box << std::endl;
+
+
+
+
+	return 0;
+}
