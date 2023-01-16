9d4b1236d69ec80ecf2846b6628fe054228df42c
Michał Maj <michalpmaj@gmail.com>
2023-01-16 21:07:56 +0100

    traversing container subset with iterators

---
+++ 40_stl_containers_iterators/04_traversing_container_subset_with_iterators/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <array>
+
+template <typename T>
+void print_collection(const T& collection, size_t begin_adjustment=0, size_t end_adjustment=0)
+{
+	// Adjusting begin and end
+	auto start_point = collection.begin() + begin_adjustment;
+	auto end_point = collection.end() - end_adjustment;
+
+	std::cout << "[";
+	while (start_point != end_point)
+	{
+		std::cout << " " << *start_point;
+		++start_point;
+	}
+	std::cout << "]" << std::endl;
+}
+
+int main()
+{
+	std::vector<int> ints1{ 11, 22, 33, 44, 55, 66, 77 };
+	std::array<int, 6> ints2{ 100, 200, 300, 400, 500, 600 };
+
+	print_collection(ints1, 2, 2);
+	print_collection(ints1, 1, 2);
+	print_collection(ints1, 1, 3);
+
+	print_collection(ints2, 1, 1);
+
+	return 0;
+}
